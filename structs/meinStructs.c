6e0efb0fa03e6527c7bfab36a3d39e1dcea246a5
euclidthedark <euclidthedark@gmail.com>
2020-07-18 23:50:05 -0400

    Learn how to create structs from factories functions.

--- structs/meinStructs.c
+++ structs/meinStructs.c
@@ -7,23 +7,33 @@ struct OrderedPair {
 };
 
 // This is defining a struct as a typedef
+// Notice that this structure is also nested.
 typedef struct Triangle {
   struct OrderedPair a;
   struct OrderedPair b;
   struct OrderedPair c;
 } Triangle;
 
+struct OrderedPair make_ordered_pair(int length)
+{
+  struct OrderedPair p1 = {0, length * 1 };
+
+  return p1;
+}
+
+// You can also create structures with functions
+// this pattern is very similar to the factory pattern
+
 int main()
 {
   struct OrderedPair p1 = { 0, 0 };
   struct OrderedPair p2 = { 0, 2 };
-  struct OrderedPair p3 = { 2, 0 };
 
-  Triangle t1 = { p1, p2, p3 };
+  Triangle t1 = { p1, p2, make_ordered_pair(2) };
 
   printf("This is p1 x and y %d, %d \n", p1.x, p1.y);
   printf(
-    "These are the ordered pairs for the triangle (%d, %d), (%d, %d), (%d, %d)",
+    "These are the ordered pairs for the triangle (%d, %d), (%d, %d), (%d, %d) \n",
     t1.a.x, t1.a.y, t1.b.x, t1.b.y, t1.c.x, t1.c.y
   );
 
